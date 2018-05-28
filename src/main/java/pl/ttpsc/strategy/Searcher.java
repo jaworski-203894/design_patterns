e2647d60db4458a691d1c8de46eb0e5a6d192b40
Marcin Jaworski <m.a.jaworski96@gmail.com>
2018-05-28 21:39:28 +0200

    Missing file

--- src/main/java/pl/ttpsc/strategy/Searcher.java
+++ src/main/java/pl/ttpsc/strategy/Searcher.java
@@ -1,4 +1,16 @@
 package pl.ttpsc.strategy;
 
-public class Searcher {
+import java.util.List;
+
+public class Searcher<T> {
+    List<? extends Comparable<? super T>> collection;
+    SearchStrategy strategy;
+
+    public Searcher(List<? extends Comparable<? super T>> collection, SearchStrategy strategy) {
+        this.collection = collection;
+        this.strategy = strategy;
+    }
+    public boolean find(T item) {
+        return strategy.search(collection, item) != -1;
+    }
 }
